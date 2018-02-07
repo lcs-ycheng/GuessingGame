45e1a22972933deb6b5a6c2700236ad77fcca94b
Yaming Cheng <ycheng@lcs.on.ca>
2018-02-07 11:02:26 -0500

    start putting the array

--- GuessingGame/GuessingGame.swift
+++ GuessingGame/GuessingGame.swift
@@ -12,14 +12,22 @@ struct GuessingGame {
     
     //Properties
     var numberToGuess : Int
+    var numberGuessed : [Int]
     
     //MARK: Initializer
     init() {
         //Generate the random number
         numberToGuess = Int(arc4random_uniform(501)) //Get a number between 0 and 500
+        
+        //make an empty list of numbers guessed
+        numberGuessed = []
     }
     //MARK: Function
-    func checkGuessGiveFeedback(guess: Int) -> String {
+   mutating func checkGuessGiveFeedback(guess: Int) -> String {
+    //Add the provided guess to the list of guessing number
+    numberGuessed.append(guess)
+        
+        //compare the guessed number with the orginal number
         if guess < numberToGuess {
             return "Guess higher next time"
         } else if guess > numberToGuess {
